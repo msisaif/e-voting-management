f17e160c5e0ceca04a7ff69477abab385e9eac84
msisaif <msi313.bd@gmail.com>
2025-04-27 20:09:18 +0600

    Add new components for user authentication and profile management
    - Implement password update functionality in `password-CQPSclzh.js`
    - Create placeholder pattern component in `placeholder-pattern-CV0IXm_K.js`
    - Develop user profile settings in `profile-ChaYP7n0.js`
    - Add user registration form in `register-Cf8jHvPZ.js`
    - Implement password reset functionality in `reset-password-ChQbicMF.js`
    - Create text link component in `text-link-Bgk_yLFD.js`
    - Add transition effects in `transition-ddzY3ZcA.js`
    - Implement email verification functionality in `verify-email-DyjhotJR.js`
    - Create welcome page component in `welcome-eEzC55Fz.js`

---
+++ public/build/assets/linkedin-CxcdcpWM.js
@@ -0,0 +1,16 @@
+import{c as a}from"./createLucideIcon-ByE9PMH6.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],h=a("ExternalLink",e);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z",key:"1jg4f8"}]],n=a("Facebook",c);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z",key:"c2jq9f"}],["rect",{width:"4",height:"12",x:"2",y:"9",key:"mk3on5"}],["circle",{cx:"4",cy:"4",r:"2",key:"bt5ra8"}]],i=a("Linkedin",o);export{h as E,n as F,i as L};
